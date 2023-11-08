c70be403ef83bc886e1feaff7cd07bcb67882aa6
moneprz <145432190+moneprz@users.noreply.github.com>
2023-11-08 08:05:02 +0100

    Update songs.php

--- songs.php
+++ songs.php
@@ -1,1 +1,15 @@
+<?php
 
+class songs
+{
+
+    private int $id;
+    private string $name;
+    private string $gameName;
+    private int $releaseYea;
+    public function __construct($id, $name, $gameName, $releaseYear, $tracklist){
+        $this->id = $id;
+        $this->name = $name;
+
+    }
+}
