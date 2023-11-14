d5a6fc8ed03a8c21b9205a24c8f97b99a13665b1
moneprz <145432190+moneprz@users.noreply.github.com>
2023-11-14 08:20:56 +0100

    Update songs.php

--- songs.php
+++ songs.php
@@ -1,15 +1,49 @@
 <?php
 
-class songs
+class songs implements JsonSerializable
 {
 
     private int $id;
     private string $name;
     private string $gameName;
     private int $releaseYea;
+    private array $tracklist;
     public function __construct($id, $name, $gameName, $releaseYear, $tracklist){
         $this->id = $id;
         $this->name = $name;
+        $this->gameName = $gameName;
+        $this->releaseYea = $releaseYear;
+        $this->tracklist = $tracklist;
+    }
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getGameName(): string
+    {
+        return $this->gameName;
+    }
+
+    public function getReleaseYea(): int
+    {
+        return $this->releaseYea;
+    }
+
+    public function getTracklist(): array
+    {
+        return $this->tracklist;
+    }
+
+
+    public function jsonSerialize(): mixed
+    {
+        // TODO: Implement jsonSerialize() method.
     }
 }
