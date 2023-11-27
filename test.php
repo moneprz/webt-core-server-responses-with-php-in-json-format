9a0c9b07df8ea5b3cc9cb54956633d0549e79335
moneprz <145432190+moneprz@users.noreply.github.com>
2023-11-27 20:23:49 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,46 @@
 <?php
-require "seeder.php";
+
+
+
+require 'seeder.php';
+
 header('Content-type: application/json');
-var_dump(Seeder::seed());
-if(isset($_GET['ost'])) {
-    foreach($ost as $soundtrack){
-        /* @var $soundtrack ost */
-        if($soundtrack->getId()==$_GET['ost']){
-            echo json_encode($ost);
+class TestOSTsApi
+{
+
+    public function handleRequest()
+    {
+        if (isset($_GET['ost_id'])) {
+            $ostId = $_GET['ost_id'];
+            $this->outputSingleOST($ostId);
+        } else {
+            $this->outputAllOSTs();
         }
     }
+
+
+    public function outputSingleOST($ostId)
+    {
+        $seeder = new Seeder();
+        $osts = $seeder->createOSTs();
+
+        foreach ($osts as $ost) {
+            if ($ost->getID() == $ostId) {
+                echo json_encode($ost, JSON_PRETTY_PRINT);
+                return;
+            }
+        }
+        echo json_encode(["error" => "OST not found"], JSON_PRETTY_PRINT);
+    }
+
+    public function outputAllOSTs()
+    {
+        $seeder = new Seeder();
+        $osts = $seeder->createOSTs();
+
+        echo json_encode($osts, JSON_PRETTY_PRINT);
+    }
 }
+
+$testOSTApi = new TestOSTsApi();
+$testOSTApi->handleRequest();
